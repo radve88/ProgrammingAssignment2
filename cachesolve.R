614dfe54386455c256a3f1365764a974b61115b7
Rashmikant Dave <rashmikant.dave@gmail.com>
2016-04-24 20:42:05 -0700

    Changes suggested by feedback from classmates
    Changes incorporated rectification of path to complete assignment
    addition of comments to R code and syntax corrections completed

--- cachesolve.R
+++ cachesolve.R
@@ -1,14 +1,21 @@
+
+#This function checks if the matrix solution is cached 
+#if it is then the cached value is returned in case 
+#there is no cached value returned then matrix solution is 
+#calculated and cached for use when function is called again
+
+
 cachesolve <- function(x = matrix(nrow = 2,ncol= 2)) 
 {
   
   
   
-  m <- x$getsolve
+  m <- x$getsolve()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  data <- x$get
+  data <- x$get()
   m <- solve(data)
   x$setsolve(m)
   m
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,9 @@
+# This function takes in a matrix which is assumed to be inversible
+# it defines functions get setsolve and getsolve to get the value of 
+# matrix passed and to calculate matrix solution and assign it to cache 
+# variable there is also a function getsolve to retrieve the cached value
+
+
 makeCacheMatrix <- function(y = matrix(nrow = 2,ncol= 2)) 
   {
   
